afa72de6aa5b9df3a4edaae516f2cd78c53eab95
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2022-01-04 00:39:29 +0900

    connect 다시 받아서 변경

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
 // libs
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import morgan from 'morgan';
-import mongoose from 'mongoose';
-import detectPort from 'detect-port';
-import chalk from 'chalk';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import detectPort from "detect-port";
+import chalk from "chalk";
 
 // api
-import auth from './api/auth.js';
-import posts from './api/posts.js';
-import docs from './utils/api-doc.js';
+import auth from "./api/auth.js";
+import posts from "./api/posts.js";
+import docs from "./utils/api-doc.js";
 
 // utils
-import { authenticateUser } from './utils/auth.js';
+import { authenticateUser } from "./utils/auth.js";
 
 // mongo db
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 mongoose.connect(
-  'mongodb+srv://test:1234@cluster0-ypgh5.mongodb.net/test?retryWrites=true&w=majority',
+  "mongodb+srv://test:1234@cluster0.qmpus.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
-  },
+  }
 );
 mongoose.Promise = global.Promise;
 
@@ -38,20 +38,20 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(morgan('dev')); // log request
+app.use(morgan("dev")); // log request
 
 // express routers
-app.use('/', auth);
-app.use('/posts', authenticateUser, posts);
+app.use("/", auth);
+app.use("/posts", authenticateUser, posts);
 
 // api docs
-app.use('/api', docs);
+app.use("/api", docs);
 
 // start
 app.listen(port, () =>
   console.log(
     `${chalk.white
-      .bgHex('#41b883')
-      .bold(`VUE TIL SERVER IS RUNNING ON ${port}`)}`,
-  ),
+      .bgHex("#41b883")
+      .bold(`VUE TIL SERVER IS RUNNING ON ${port}`)}`
+  )
 );
